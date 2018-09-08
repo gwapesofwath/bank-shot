f173b603c91a03061371ca3b561656478e8368d0
gwapesofwath <gwapesofwath@gmail.com>
2018-09-08 12:29:54 -0500

    user auth

---
+++ client/js/app/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <hr />
+        <p>Dis here is the end of our page</p>
+    </footer>
+);
+
+export default Footer;
---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    db: 'mongodb://localhost/database',
+    db_dev: 'mongodb://localhost/database',
+    JWT_SECRET: 'DON',
+};
+
